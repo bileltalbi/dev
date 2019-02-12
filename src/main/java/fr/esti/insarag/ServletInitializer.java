f542263e6134334179a7f7d0b36da99b94dd45b1
TALBi Bilel <bilel.talbi@externe.mousquetaires.com>
2019-02-12 17:23:43 +0100

    first commit

---
+++ src/main/java/fr/esti/insarag/ServletInitializer.java
@@ -0,0 +1,14 @@
+package fr.esti.insarag;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(InsaragApplication.class);
+    }
+
+}
+
